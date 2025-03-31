6cd0eb603bc3b8ee8bd2647bd869dab69af9c688
Ruona125 <ogheneruonaagadagba4@gmail.com>
2025-03-31 21:26:18 +0100

    changed the mongodb url

--- src/services/mongo.js
+++ src/services/mongo.js
@@ -1,24 +1,24 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
+require('dotenv').config()
+const MONGO_URL = process.env.MONGO_URL
 
-const MONGO_URL = "mongodb://mongo:aGa323BC-CAd4EhCgedb36aDaFD35b1b@roundhouse.proxy.rlwy.net:21970"
+mongoose.connection.once('open', () => {
+  console.log('MongoDB connection ready')
+})
 
-mongoose.connection.once("open", () => {
-  console.log("MongoDB connection ready");
-});
+mongoose.connection.on('error', err => {
+  console.error(err)
+})
 
-mongoose.connection.on("error", (err) => {
-  console.error(err);
-});
-
-async function mongoConnect() {
-  await mongoose.connect(MONGO_URL);
+async function mongoConnect () {
+  await mongoose.connect(MONGO_URL)
 }
 
-async function mongoDisconnect() {
-  await mongoose.disconnect();
+async function mongoDisconnect () {
+  await mongoose.disconnect()
 }
 
 module.exports = {
   mongoConnect,
-  mongoDisconnect,
-};
+  mongoDisconnect
+}
